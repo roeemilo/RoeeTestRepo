7627ba26674ce3e531427a2dcc205aab876c1f02
roeemilo <roeemilo1@gmail.com>
2022-08-16 18:49:03 +0300

    change1

---
+++ roee-test-git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class roeeClass
+{
+    public static void printMyName(string name)
+    {
+        Console.WriteLine($"my name is {name}");
+    }
+
+    public static void Main (string[] args)
+    {
+        string name = Console.ReadLine();
+        printMyName(name);
+    }
+}
